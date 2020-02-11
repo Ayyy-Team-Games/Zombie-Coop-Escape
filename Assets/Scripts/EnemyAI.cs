e8624320652bbdf1f43ab17f31d6e8890ca1f15d
Shawn Antonucci <shawnantonucci2018@gmail.com>
2020-02-11 01:56:18 -0500

    basic enemy ai

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform player;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+        var distance = Vector3.Distance(agent.transform.position, player.transform.position);// no need to perform this operation twice.
+        if (distance < 20)
+            agent.SetDestination(player.transform.position);
+    }
+}
