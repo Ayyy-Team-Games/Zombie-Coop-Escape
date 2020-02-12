e811952685f685ef52b86e567b8917d611eee7dc
Guy Ross <guyallenross@gmail.com>
2020-02-11 19:00:21 -0500

    stff

---
+++ MultiplayerServer/MultiplayerServer/Common/Vector3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiplayerServer.Common
+{
+    public class Vector3
+    {
+        private readonly Tuple<float, float, float> _vectors;
+
+        public float X => _vectors.Item1; 
+        public float Y => _vectors.Item2;
+        public float Z => _vectors.Item3;
+
+        public Vector3(int x, int y, int z)
+        {
+            _vectors = new Tuple<float, float, float>(x, y, z);
+        }
+    }
+}
